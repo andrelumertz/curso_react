8a919038b9e82ed90afd1c0aa238c4271c8e5325
André Martins <69599938+andrelumertz@users.noreply.github.com>
2021-07-08 14:05:40 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './App.css';
+
+
+const App = () => {
+  
+let msg = "My name is André !! ";
+  return <div className="container">{msg}</div>; 
+};
+
+export default App;
